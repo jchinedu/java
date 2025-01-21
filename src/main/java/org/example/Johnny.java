cdb9ab9733217effcbcc664c18d0ab5d8db89958
jchinedu <you@example.com>
2025-01-21 14:22:28 +0100

    my first class

---
+++ src/main/java/org/example/Johnny.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class Johnny {
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+}
---
+++ src/main/java/org/example/Simba.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Simba {
+    public static void main(String[] args) {
+        System.out.println("SIMBA");
+    }
+}
