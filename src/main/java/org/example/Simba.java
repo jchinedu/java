572880f97f60aa642d4b2c56f38ccc5dff87917e
jchinedu <you@example.com>
2025-01-21 16:48:04 +0100

    git

--- src/main/java/org/example/Simba.java
+++ src/main/java/org/example/Simba.java
@@ -2,6 +2,17 @@ package org.example;
 
 public class Simba {
     public static void main(String[] args) {
-        System.out.println("SIMBA");
+        System.out.println("Hello, John");
+
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("it is not an alien!");
+            System.out.println("And i am scared of aliens");
+        }
+
+        int topScore = 100;
+        if (topScore == 100) {
+            System.out.println("you got the high score!");
+        }
     }
 }
