eeb10353ce1d4d73766f478626b0e33847bfd4d0
jchinedu <you@example.com>
2025-01-23 05:47:23 +0100

    git

--- src/main/java/org/example/Johnny.java
+++ src/main/java/org/example/Johnny.java
@@ -1,5 +1,20 @@
 package org.example;
 
 public class Johnny {
-    public static void
+    public static void main(String[] args) {
+         double firstVariable = 20;
+         double secondVariable = 85;
+         double result = (firstVariable + secondVariable) * 100;
+          double Remainder = result % 40;
+
+         boolean isZero = (Remainder == 0) ;
+         if (isZero) {
+             System.out.println("true");}
+         if (!isZero) {
+             System.out.println("got some remainder");
+         }
+
+
+
+    }
 }
