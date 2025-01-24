8af770147c248945413f8cec408fd5118db98bd3
jchinedu <you@example.com>
2025-01-24 06:17:41 +0100

    git

---
+++ src/main/java/Jce.java
@@ -0,0 +1,12 @@
+public class Jce {
+    public static void main(String[] args) {
+       int myVariable = 50;
+
+       myVariable++;
+       myVariable--;
+
+       System.out.println("this is a test");
+        int anothervariable = 50;
+        myVariable--;
+        System.out.println("myVariable = " + myVariable);
+    }
