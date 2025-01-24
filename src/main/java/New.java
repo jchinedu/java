8d484f8c53f2edeb93ed00e755f85564ac361b95
jchinedu <you@example.com>
2025-01-24 17:11:55 +0100

    git

--- src/main/java/New.java
+++ src/main/java/New.java
@@ -6,9 +6,29 @@ public class New {
         int bonus = 200;
         int finalscore = score;
 
-        if (gameon == true) {
+        johnny(true, 10000, 8, 200);
+
+        score = 1000;
+        levelcompleted = 8;
+        bonus = 200;
+
+        finalscore = score;
+        if (gameon) {
             finalscore += (levelcompleted * bonus);
-            System.out.println("your final score was " + finalscore);
+            System.out.println("your final score was" + finalscore);
         }
+
+//        if (gameon == true) {
+//            finalscore += (levelcompleted * bonus);
+//            System.out.println("your final score was " + finalscore);
+//        }
+    }
+    public static void johnny(boolean gameon, int score, int levelcompleted, int bonus) {
+
+    int finalscore = score;
+
+        if (gameon == true) {
+        finalscore += (levelcompleted * bonus);
+        System.out.println("your final score was " + finalscore);}
     }
 }
