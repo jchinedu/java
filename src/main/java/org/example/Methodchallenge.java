e55d2f76e683bfdfbfe0e8069f0524ebe0c0b360
jchinedu <you@example.com>
2025-01-28 05:18:03 +0100

    git

---
+++ src/main/java/org/example/Methodchallenge.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Methodchallenge {
+    public static void main(String[] args){
+        System.out.println("68in = " + convertToCentimeters(68) + "cm");
+        System.out.println("5ft, 8in = " + convertToCentimeters(5, 8) + "cm");
+    }
+    public static double convertToCentimeters(int inches) {
+    return inches * 2.54;
+    }
+    public static double convertToCentimeters(int feet, int inches ) {
+    return ((feet * 12) + inches) * 2.54;
+    }
+}
