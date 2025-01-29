743299b659d13d02c178bcdfc54002a03a67e77e
jchinedu <you@example.com>
2025-01-29 10:14:18 +0100

    git

--- src/main/java/org/example/SecondsandMinuteschallenge.java
+++ src/main/java/org/example/SecondsandMinuteschallenge.java
@@ -2,28 +2,39 @@ package org.example;
 
 public class SecondsandMinuteschallenge {
     public static void main(String[] args) {
-        System.out.println(" 1m = " + getDurationString(60) + "s");
-        System.out.println("1hr, 2m =" + getDurationString(60 * 60, 60));
-        int minutes = 60;
-        int hours = 60 * 60;
-        int seconds = 1;
+        System.out.println(getDurationString(-3945));
+        System.out.println(getDurationString(-65, 45));
+        System.out.println(getDurationString(65, 145));
+        System.out.println(getDurationString(65, 45));
+        System.out.println(getDurationString(3945));
+
+
     }
-    public static double getDurationString(int seconds) {
-        if (seconds >= 0) {
-            System.out.println("we are good");
+    public static String getDurationString(int seconds) {
+        if (seconds < 0) {
+            return "invalid data for seconds(" + seconds + "), must be a positive integer value";
+
         }
-        return seconds;
+        int minutes = seconds / 60;
+        return getDurationString(seconds / 60, seconds % 60);
 
     }
-    public static double getDurationString(int minutes, int seconds) {
-        if (minutes >= 0) {
-            System.out.println("we might be good");
-        } else if (seconds <= 59) {
-            System.out.println("brag about it then");
-        } else
-            System.out.println("nice");
 
-        return getDurationString(60*60) + (60);
+    public static String getDurationString(int minutes, int seconds) {
+        if (minutes < 0) {
+            return "invalid data for minutes(" + minutes + "), must be a positive integer value";
 
+        }
+        if (seconds < 0 || seconds > 59){
+            return "invlid data for seconds(" + seconds + "), ,must be between 0 and 59";
+        }
+        int hours = minutes / 60;
+        int remainingMinutes = minutes % 60;
+
+        int remainingSeconds = seconds % 60;
+
+
+        return  hours + "h" + remainingMinutes + "m" + remainingSeconds + "s";
     }
+
 }
