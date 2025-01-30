1ba5072b4fc59808ef9965e087a66ad42954b0e0
jchinedu <you@example.com>
2025-01-30 19:13:37 +0100

    git

--- src/main/java/org/example/Primenumberchallenge.java
+++ src/main/java/org/example/Primenumberchallenge.java
@@ -2,13 +2,12 @@ package org.example;
 
 public class Primenumberchallenge {
     public static void main(String[] args){
-        System.out.println("0 is " + (isprime(0) ? "": "NOT ") + " a prime number");
-        System.out.println("1 is " + (isprime(1) ? "": "NOT ") + " a prime number");
-        System.out.println("2 is " + (isprime(2) ? "": "NOT ") + " a prime number");
-        System.out.println("3 is " + (isprime(3) ? "": "NOT ") + " a prime number");
-        System.out.println("8 is " + (isprime(8) ? "": "NOT ") + " a prime number");
-        System.out.println("17 is " + (isprime(17) ? "": "NOT ") + " a prime number");
-
+        int count = 0;
+        for (int i =10; i <= 50; i++) {
+            if (isprime(i)) {
+                System.out.println("number " + i + "is a prime number");
+            }
+        }
     }
     public static boolean isprime(int wholeNumber) {
         if (wholeNumber <= 2) {
