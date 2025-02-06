a0155a155f65623450be692c9be8e6dc4e24bc5e
jchinedu <you@example.com>
2025-02-06 12:55:34 +0100

    git

---
+++ src/main/java/Car.java
@@ -0,0 +1,34 @@
+public class Car {
+    private String make = "Tesla";
+    private String model = "Model x";
+    private String color = "Gray";
+    private int doors =2;
+    private boolean convertible = true;
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public boolean isConvertible() {
+        return convertible;
+    }
+
+    public void describeCar() {
+        System.out.println(doors + "-Door " +
+                color + " " +
+                make + " " +
+                model + " " +
+                (convertible ? "convertible" : ""));
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        Car car = new Car();
+//        car.make = "porsche";
+//        car.model = "Carrera";
+//        car.color = "Red";
+        System.out.println("make = " + car.getMake());
+        System.out.println("model = " + car.getModel());
+        car.describeCar();
+    }
+}
